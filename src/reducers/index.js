c5185d12ed47e3afe60d8df3dc348634b49bb46b
AshishBansal2101 <bansalashish2101@gmail.com>
2021-11-14 17:00:17 +0530

    adding search results and add to movies option

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,55 +1,80 @@
-import { combineReducers } from 'redux';
-import {ADD_MOVIES, ADD_TO_FAVOURITES,REMOVE_FROM_FAVOURITES, SET_SHOW_FAVOURITES} from '../actions'
+import { combineReducers } from "redux";
+import {
+  ADD_MOVIES,
+  ADD_TO_FAVOURITES,
+  REMOVE_FROM_FAVOURITES,
+  SET_SHOW_FAVOURITES,
+  ADD_SEARCH_RESULT,
+  ADD_MOVIE_TO_LIST,
+} from "../actions";
 
-const initialMoviesState={
-    list:[],
-    favourites:[],
-    showFavourites:false
+const initialMoviesState = {
+  list: [],
+  favourites: [],
+  showFavourites: false,
 };
-export function movies(state=initialMoviesState,action){
-    console.log('MOVIES REDUCER');
-    switch(action.type){
-        case ADD_MOVIES:
-            return {
-                ...state,
-                list:action.movies
-            }
-        case ADD_TO_FAVOURITES:
-            return {
-                ...state,
-                favourites: [action.movie,...state.favourites]
-            }
-        case REMOVE_FROM_FAVOURITES:
-            const filteredArray=state.favourites.filter(
-                movie=>movie.Title!==action.movie.Title
-            );
-
-            return {
-                ...state,
-                favourites:filteredArray
-            }
-        case SET_SHOW_FAVOURITES:
-            return {
-                ...state,
-                showFavourites:action.val
-            }
-        default:
-            return state;
-    }
+export function movies(state = initialMoviesState, action) {
+  console.log("MOVIES REDUCER");
+  switch (action.type) {
+    case ADD_MOVIES:
+      return {
+        ...state,
+        list: action.movies,
+      };
+    case ADD_TO_FAVOURITES:
+      return {
+        ...state,
+        favourites: [action.movie, ...state.favourites],
+      };
+    case REMOVE_FROM_FAVOURITES:
+      const filteredArray = state.favourites.filter(
+        (movie) => movie.Title !== action.movie.Title
+      );
+      return {
+        ...state,
+        favourites: filteredArray,
+      };
+    case SET_SHOW_FAVOURITES:
+      return {
+        ...state,
+        showFavourites: action.val,
+      };
+    case ADD_MOVIE_TO_LIST:
+      return {
+        ...state,
+        list: [action.movie, ...state.list],
+      };
+    default:
+      return state;
+  }
 }
 
-const initialSearchState={
-    result:{}
+const initialSearchState = {
+  result: {},
+  showSearchResults: false,
 };
-export function search(state=initialSearchState,action){
-    console.log('SEARCH REDUCER');
-    return state;
+export function search(state = initialSearchState, action) {
+  switch (action.type) {
+    case ADD_SEARCH_RESULT:
+      return {
+        ...state,
+        result: action.movie,
+        showSearchResults: true,
+      };
+    case ADD_MOVIE_TO_LIST:
+      return {
+        ...state,
+        showSearchResults:false
+      };
+    default:
+      return state;
+  }
 }
 
-const initialRootState={
-    movies:initialMoviesState,
-    search:initialSearchState
-}
+// const initialRootState = {
+//   movies: initialMoviesState,
+//   search: initialSearchState,
+// };
 // export default function rootReducer(state=initialRootState,action){
 //     return {
 //         movies:movies(state.movies,action),
@@ -58,8 +83,8 @@ const initialRootState={
 // }
 
 export default combineReducers({
-    // movies:movies,
-    // search:search
-    movies,
-    search
+  // movies:movies,
+  // search:search
+  movies,
+  search,
 });
