45bfbcb47f94e59757e990b79495129a830727b7
AshishBansal2101 <bansalashish2101@gmail.com>
2021-11-16 16:39:36 +0530

    adding context api

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { StoreContext } from "../index";
 // import {data} from '../data';
 import { addMovieToList ,handleMoviesSearch } from '../actions';
 
@@ -53,4 +54,16 @@ handleChange=(e)=>{
   }
 }
 
-export default Navbar;
+class NavbarWrapper extends React.Component{
+  render() {
+    return (
+      <StoreContext.Consumer>
+        {(store)=>(
+        <Navbar dispatch={store.dispatch} search={this.props.search}/>
+        )}
+      </StoreContext.Consumer>
+    )
+  }
+}
+
+export default NavbarWrapper;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import {createStore,applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
@@ -38,6 +38,20 @@ const logger=({dispatch,getState})=>(next)=>(action)=>{
 
 const store=createStore(rootReducer,applyMiddleware(logger,thunk)); 
 // console.log('store',store);
+console.log('state',store.getState());
+
+export const StoreContext=createContext();
+
+console.log('StoreContext',StoreContext);
+
+class Provider extends React.Component{
+  render() {
+    const { store } = this.props;
+    return <StoreContext.Provider value={store}>
+      {this.props.children}
+    </StoreContext.Provider>;
+  }
+}
 // console.log('BEFORE STATE',store.getState());
 
 // store.dispatch({
@@ -47,8 +61,8 @@ const store=createStore(rootReducer,applyMiddleware(logger,thunk));
 
 // console.log('AFTER STATE',store.getState());
 ReactDOM.render(
-  <React.StrictMode>
-    <App store={store} />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
